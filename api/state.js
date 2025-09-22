49c79ff2ff227a39723aed0ffd0394dfde007444
daengdul2 <106536045+daengdul2@users.noreply.github.com>
2025-09-23 03:05:05 +0800

    Update state.js

--- api/state.js
+++ api/state.js
@@ -3,8 +3,9 @@ import { redis } from "./_redis";
 export default async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const roomId = url.searchParams.get("room");
-  const room = await redis.get(`room:${roomId}`);
-  if (!room) return res.json({ error: "Room tidak ditemukan" });
 
-  res.json(room);
+  const raw = await redis.get(`room:${roomId}`);
+  if (!raw) return res.status(400).json({ error: "Room tidak ada" });
+
+  res.status(200).json(JSON.parse(raw));
 };

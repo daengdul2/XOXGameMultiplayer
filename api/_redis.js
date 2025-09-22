22bd04f04a6454f009e6e4e9045c44afcf3508a2
daengdul2 <106536045+daengdul2@users.noreply.github.com>
2025-09-23 03:11:15 +0800

    Update _redis.js

--- api/_redis.js
+++ api/_redis.js
@@ -1,8 +1,8 @@
-import { Redis } from "@upstash/redis";
-
-export const redis = new Redis({
+import { Redis } from '@upstash/redis'
+const redis = Redis.fromEnv()
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 })
-  await redis.set("foo", "bar"),
-    await redis.get("foo");
+
+await redis.set("foo", "bar");
+await redis.get("foo");
